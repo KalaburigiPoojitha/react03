dc9c03da74df4a0a92862d313b1c3297aebb9c50
KalaburigiPoojitha <kalaburigipoojitha05@gmail.com>
2024-06-24 14:28:34 +0530

    Add files via upload

---
+++ react3/src/App.js
@@ -0,0 +1,179 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src="./images/good-vibes.avif" alt=""></img>
+      <div className="headContainer">
+        <div className="navBar">FACE</div>
+        <div className="navBar">BODY</div>
+        <div className="navBar">MAKEUP</div>
+        <div className="navBar">HAIR</div>
+        <div className="navBar">BLOGS</div>
+        <div className="navBar">REWARDS</div>
+        <div className="navBar">REFERRAL</div>
+      </div>
+      <hr></hr>
+
+      <div className="home">Home/All Products</div>
+      <div>
+        <h2>All Products</h2>
+      </div>
+      <div>
+        <img src="./images/GV_banner1.webp" alt=""></img>
+      </div>
+      <div className="mainContainer">
+        <div className="container">
+          <div className="container1">
+            <h3>Best Sellers</h3>
+            <img className="image" src="./images/product1.webp" alt=""></img>
+            <h5>De-Tan Glow Ubtan Face Serum</h5>
+            <p>Rs: 325</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product2.webp" alt=""></img>
+            <h5>Anti-Blemish Glow Gel Creme With Power Of Serum</h5>
+            <p>Rs: 499</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product3.webp" alt=""></img>
+            <h5>Hairfall Control Onion Scalp Serum</h5>
+            <p>Rs: 399</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product4.webp" alt=""></img>
+            <h5>Smoothening Hair Serum Keratin</h5>
+            <p>Rs: 275</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product5.webp" alt=""></img>
+            <h5>Chamomile Moisture Rich Shower Gel Body Wash</h5>
+            <p>Rs: 275</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product6.webp" alt=""></img>
+            <h5>Soothing Micellar Water with Aloevera</h5>
+            <p>Rs: 199</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product7.webp" alt=""></img>
+            <h5>Tinted Red Onion Sunscreen SPF50 </h5>
+            <p>Rs: 425</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product8.webp" alt=""></img>
+            <h5>Keratin Noursshing Conditioner For Smoother Hair</h5>
+            <p>Rs: 325</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product9.webp" alt=""></img>
+            <h5>De-Tan Glow Light Day Cream</h5>
+            <p>Rs: 499</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product10.webp" alt=""></img>
+            <h5>Banana Shine Hair Serum</h5>
+            <p>Rs: 275</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product11.webp" alt=""></img>
+            <h5>Papaya Even Skin Tone Night Cream with Power of Serum</h5>
+            <p>Rs: 275</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product12.webp" alt=""></img>
+            <h5>Strawberry Cooling Gel Body Lotion</h5>
+            <p>Rs: 299</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product13.webp" alt=""></img>
+            <h5>Aloe Vera Cooling Gel Body Lotion</h5>
+            <p>Rs: 299</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product15.webp" alt=""></img>
+            <h5>Hydrating Rose Face cream with Rose Power Of Serum</h5>
+            <p>Rs: 349</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product14.webp" alt=""></img>
+            <h5>Rose Cooling Gel Body Wash</h5>
+            <p>Rs: 299</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+
+          <div className="container1">
+            <img className="image" src="./images/product16.webp" alt=""></img>
+            <h5>Hydrating glow Night Cream Rosehip</h5>
+            <p>Rs: 349</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product17.webp" alt=""></img>
+            <h5>Vitamin C Day Cream SPF30 with Power of Serum</h5>
+            <p>Rs: 425</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+          <div className="container1">
+            <img className="image" src="./images/product18.webp" alt=""></img>
+            <h5>Glow Light Moisturiser</h5>
+            <p>Rs: 229</p>
+            <div className="cart">ADD TO CART</div>
+          </div>
+        </div>
+        <div className="foot">
+          <div className="footContainer">
+            <div>
+              <img src="./images/GV_Logo.avif" alt=""></img>
+            </div>
+            <div>
+              Manash Lifestyle Pvt. Ltd., 101 B, Raheja Plaza, L.B.S. Road,
+              Ghatkopar (West), Mumbai - 400086
+            </div>
+            <p>+91-7208-900-111</p>
+            <p>care@goodvibesonly.in</p>
+          </div>
+          <div>
+            <div>OUR OTHER BRANDS</div>
+            <div>FACESCANADA</div>
+            <div>CARMESI</div>
+          </div>
+          <div>
+            <div>INFORMATION</div>
+            <p>Search</p>
+            <p>About Us</p>
+            <p>Terms of Service</p>
+            <p>Refund Policy</p>
+          </div>
+          <div>
+            <div>TOP CATEGORIES</div>
+            <p>Skincare</p>
+            <p>Hair Care</p>
+            <p>Bath and Body</p>
+            <p>Oils</p>
+            <p>Ingredients</p>
+            <p>Blogs</p>
+          </div>
+        </div>
+        <foot>© 2024 GOOD VIBES ALL RIGHTS RESERVED</foot>
+      </div>
+    </div>
+  );
+}
+
+export default App;
